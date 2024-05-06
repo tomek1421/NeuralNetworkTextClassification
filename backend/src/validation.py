d6de0e1866627029c39ad83d2f9eec1c071962fd
tomek_1421 <t.wojciechowski.648@studms.ug.edu.pl>
2024-05-06 20:46:07 +0200

    add validation error messages for check diabetes route

--- backend/src/validation.py
+++ backend/src/validation.py
@@ -5,63 +5,63 @@ class CheckDiabetes(Schema):
     pregnancies = fields.Integer(
         required=True,
         validate=[
-            validate.Range(min=0, error=""),
-            validate.Range(max=20, error="")
+            validate.Range(min=0, error="Pregnancies number must be greater than or equal 0"),
+            validate.Range(max=20, error="Pregnancies number must be less than or equal 20")
         ]
     ) # 1-20
 
     glucose = fields.Integer(
         required=True,
         validate=[
-            validate.Range(min=0, error=""),
-            validate.Range(max=200, error="")
+            validate.Range(min=0, error="Glucose concentration must be greater than or equal 0"),
+            validate.Range(max=200, error="Glucose concentration must be less than or equal 200")
         ]
     ) # 0-200 Plasma glucose concentration over 2 hours in an oral glucose tolerance test
 
     blood_presure = fields.Integer(
         required=True,
         validate=[
-            validate.Range(min=0, error=""),
-            validate.Range(max=130, error="")
+            validate.Range(min=0, error="Blood presure must be greater than or equal 0"),
+            validate.Range(max=130, error="Blood presure must be less than or equal 130")
         ]
     ) # 0-130 Diastolic blood pressure (mm Hg)
     
     skin_thickness = fields.Integer(
         required=True,
         validate=[
-            validate.Range(min=0, error=""),
-            validate.Range(max=100, error="")
+            validate.Range(min=0, error="Skin thickness must be greater than or equal 0"),
+            validate.Range(max=100, error="Skin thickness must be less than or equal 100")
         ]
     ) # 0-100 Triceps skin fold thickness (mm)
 
     insulin = fields.Integer(
         required=True,
         validate=[
-            validate.Range(min=0, error=""),
-            validate.Range(max=900, error="")
+            validate.Range(min=0, error="Insulin must be greater than or equal 0"),
+            validate.Range(max=900, error="Insulin must be greater than or equal 900")
         ]
-    ) # 0-900 2-Hour serum insulin (weight in kg/(height in m)2)
+    ) # 0-900 2-Hour serum insulin (mu U/ml)
 
     bmi = fields.Float(
         required=True,
         validate=[
-            validate.Range(min=0, error=""),
-            validate.Range(max=70, error=""),
+            validate.Range(min=0, error="BMI must be greater than or equal 0"),
+            validate.Range(max=70, error="BMI must be less than or equal 70"),
         ]
-    ) # 0-70 Body mass index
+    ) # 0-70 Body mass index (weight in kg/(height in m)2)
     
     diabetes = fields.Float(
         required=True,
         validate=[
-            validate.Range(min=0.08, error=""),
-            validate.Range(max=2.50, error=""),
+            validate.Range(min=0, error="Diabetes pedigree function must be greater than or equal 0"),
+            validate.Range(max=2.50, error="Diabetes pedigree function must be less than or equal 2.5"),
         ]
     ) # 0.08-2.50 Diabetes pedigree function (a function which scores likelihood of diabetes based on family history)
     
     age = fields.Integer(
         required=True,
         validate=[
-            validate.Range(min=0, error=""),
-            validate.Range(max=100, error="")
+            validate.Range(min=0, error="Age must be greater than or equal 0"),
+            validate.Range(max=100, error="Age must be less than or equal 100")
         ]
     )
