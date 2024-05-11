6a22539284e20d81aba64e90cc3a5e2db9a7e43e
Tomasz Wojciechowski <t.wojciechowski.648@studms.ug.edu.pl>
2024-05-11 19:51:28 +0200

    navigation bar

---
+++ frontend/neural-network/src/components/Layout.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Outlet, NavLink } from 'react-router-dom';
+ 
+export function Layout() {
+    
+    return (
+        <div>
+            <header className="">
+                <nav>
+                    <ul className="navbar">
+                        <li><NavLink to="/" >Home</NavLink></li>
+                        <li><NavLink to="/diabetes" >Diabetes</NavLink></li>
+                        <li><NavLink to="/heart" >Heart</NavLink></li>
+                        <li><NavLink to="/model" >Model</NavLink></li>
+                    </ul>
+                </nav>
+            </header>
+            <main>
+                <Outlet />
+            </main>
+        </div>
+    )
+}
